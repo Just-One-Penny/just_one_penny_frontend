de5227149e12655494fa036d6dcaa7a19f742dff
Nate Finger <natefinger@Nates-MacBook-Pro.local>
2020-08-11 09:55:07 -0400

    use new component lib

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -4,75 +4,34 @@
  *
  */
 import React, { memo } from 'react';
-import { PortalWithState } from 'react-portal';
+import { Modal as UIModal, useModalState } from '@welcome-ui/modal';
+import { Button } from '@welcome-ui/button';
+
+enum Size {
+  sm = 'sm',
+  md = 'md',
+  lg = 'lg',
+  auto = 'auto',
+}
 
 interface Props {
   buttonElement: React.ReactNode;
   modalBody: React.ReactNode;
   modalTitle?: string;
   dialog?: boolean;
+  size?: Size;
 }
 
 export const Modal = memo((props: Props) => {
+  const modal = useModalState();
   return (
-    <PortalWithState closeOnOutsideClick closeOnEsc>
-      {({ openPortal, closePortal, isOpen, portal }) => (
-        <>
-          <div onClick={openPortal}>{props.buttonElement}</div>
-          {props.dialog
-            ? portal(
-                <>
-                  <div
-                    className="animated justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0  z-50 outline-none focus:outline-none"
-                    onClick={closePortal}
-                  >
-                    <div className="relative w-auto my-6 mx-auto">
-                      {/*content*/}
-                      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                        {/*header*/}
-                        <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                          <h3 className="text-3xl font-semibold">
-                            {props.modalTitle}
-                          </h3>
-                          <button
-                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                            onClick={closePortal}
-                          >
-                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                              ×
-                            </span>
-                          </button>
-                        </div>
-                        {/*body*/}
-                        {props.modalBody}
-                        {/*footer*/}
-                        <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
-                          <button
-                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
-                            type="button"
-                            style={{ transition: 'all .15s ease' }}
-                            onClick={closePortal}
-                          >
-                            Close
-                          </button>
-                          <button
-                            className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                            type="button"
-                            style={{ transition: 'all .15s ease' }}
-                            onClick={closePortal}
-                          >
-                            Save Changes
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                </>,
-              )
-            : portal(props.modalBody)}
-        </>
-      )}
-    </PortalWithState>
+    <>
+      <UIModal.Trigger as={Button} {...modal}>
+        {props.buttonElement}
+      </UIModal.Trigger>
+      <UIModal {...modal} size={props.size}>
+        <UIModal.Content>{props.modalBody}</UIModal.Content>
+      </UIModal>
+    </>
   );
 });
