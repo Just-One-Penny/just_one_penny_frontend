4ab282c45690155f7ea6b63eaa4e2f1c4758b3c6
Tyler Eaden <tyler.eaden@gmail.com>
2020-09-21 19:04:38 -0400

    Added Custom Styling For DonationModal React Components

--- src/app/containers/DonationModal/index.tsx
+++ src/app/containers/DonationModal/index.tsx
@@ -5,12 +5,12 @@
  */
 
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components/macro';
+import { Input } from 'app/components/Input';
 
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
 import { reducer, sliceKey, actions } from './slice';
-import { selectAmount } from './selectors';
 import { donationModalSaga } from './saga';
 
 import { useAuth } from 'context/auth-context';
@@ -31,8 +31,6 @@ export function DonationModal(props: Props) {
   };
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const amount = useSelector(selectAmount);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const dispatch = useDispatch();
 
   const onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,7 +43,7 @@ export function DonationModal(props: Props) {
     }
   };
 
-  const submitModal = (evt?: React.FormEvent<HTMLFormElement>) => {
+  const submitPayment = (evt?: React.FormEvent<HTMLFormElement>) => {
     /* istanbul ignore next  */
     if (evt !== undefined && evt.preventDefault) {
       evt.preventDefault();
@@ -55,33 +53,98 @@ export function DonationModal(props: Props) {
   };
 
   return (
-    <>
-      <Div>
-        <h1>Donate to {props.charityName}</h1>
-        <p>
-          Sunt ad commodo ex magna laboris id sint exercitation cupidatat. Enim
-          anim dolore nostrud ad nulla incididunt magna qui do qui proident
-          cupidatat quis. Sit ad tempor veniam veniam cupidatat anim ullamco
-          voluptate commodo exercitation. Veniam culpa occaecat nulla dolore
-          mollit nostrud aliquip ullamco est ex deserunt. Proident labore est
-          dolor consectetur minim anim incididunt eu ad mollit ipsum elit.
-        </p>
-        $
-        <input
-          name="amount"
-          type="number"
-          value={amount}
-          onChange={onChange}
-          min="0.01"
-          step="0.01"
-          placeholder="0.01"
-          required
-        />
-        <button onClick={() => submitModal()}>Donate</button>
-        <button>Cancel</button>
-      </Div>
-    </>
+    <Wrapper>
+      <Header>Donate to {props.charityName}</Header>
+      <Text>
+        Sunt ad commodo ex magna laboris id sint exercitation cupidatat. Enim
+        anim dolore nostrud ad nulla incididunt magna qui do qui proident
+        cupidatat quis. Sit ad tempor veniam veniam cupidatat anim ullamco
+        voluptate commodo exercitation. Veniam culpa occaecat nulla dolore
+        mollit nostrud aliquip ullamco est ex deserunt. Proident labore est
+        dolor consectetur minim anim incididunt eu ad mollit ipsum elit.
+      </Text>
+      $
+      <Input
+        name="amount"
+        type="number"
+        onChange={onChange}
+        min="0.01"
+        step="0.01"
+        placeholder="0.01"
+        required
+      />
+      <DonateButton onClick={() => submitPayment()}>Donate</DonateButton>
+      <CancelButton>Cancel</CancelButton>
+    </Wrapper>
   );
 }
 
-const Div = styled.div``;
+const Header = styled.h2`
+  text-align: center;
+  font: normal normal 900 70px/96px Avenir;
+  letter-spacing: 0px;
+  color: ${p => p.theme.text};
+  opacity: 1;
+`;
+
+const Text = styled.p`
+  text-align: left;
+  font: normal normal normal 16px/22px Avenir;
+  letter-spacing: 0px;
+  color: #333333;
+  opacity: 1;
+`;
+
+const DonateButton = styled.button`
+  background-color: ${p => p.theme.primary};
+  cursor: pointer;
+  text-decoration: none;
+  display: flex;
+  padding: 0.25rem 1rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  align-items: center;
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.4;
+  }
+  .icon {
+    margin-right: 0.25rem;
+  }
+`;
+
+const CancelButton = styled.button`
+  background-color: #c14200;
+  cursor: pointer;
+  text-decoration: none;
+  display: flex;
+  padding: 0.25rem 1rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  align-items: center;
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.4;
+  }
+  .icon {
+    margin-right: 0.25rem;
+  }
+`;
+
+const Wrapper = styled.div`
+  top: 50px;
+  left: 415px;
+  width: 450px;
+  height: 700px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 5px;
+  opacity: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
