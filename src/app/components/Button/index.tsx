360fd41b9fbcbe582eb36ee0bdff3c40facfb26f
Juan Torres <juanet1453@gmail.com>
2020-10-14 01:05:04 -0400

    created a handleButtonType function for button props

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -14,27 +14,56 @@ export function Button(props) {
   return <StyleButton {...props}></StyleButton>;
 }
 
+const handleButtonType = btnStyle => {
+  switch (btnStyle) {
+    case 'primary':
+      return `
+        color: #fff;
+        background: transparent linear-gradient(0deg, #11569B 0%, #54AECC 100%, #6CCDDD 100%, #7AE0E8 100%) 0% 0% no-repeat padding-box;
+
+        &:hover,
+        &:active {
+          background: #0A559E 0% 0% no-repeat padding-box;
+        }
+
+        &:focus {
+          text-decoration: underline;
+        }
+    `;
+
+    case 'secondary':
+      return `
+        color: #0A559E;
+        background: transparent;
+        border: 2px solid #0A559E;
+        &:hover,
+        &:active {
+          border-color: #0C4379;
+          color: #0C4379;
+        }
+
+        &:focus,
+        &:active {
+          text-decoration: underline;
+        }
+        `;
+
+    default:
+      return '';
+  }
+};
+
 export const StyleButton = styled.button<ButtonProps>`
+  ${props => handleButtonType(props.btnStyle)}
+
   width: 170px;
-  // testing props;
-  // width: ${props => (props.btnStyle === 'primary' ? '500px;' : '170px;')}
   height: 44px;
-  background: transparent
-     linear-gradient(0deg, #11569b 0%, #54aecc 100%, #6ccddd 100%, #7ae0e8 100%)
-    0% 0% no-repeat padding-box;
+  font-weight: 900;
   box-shadow: 0px 2px 6px #0000005a;
   border-radius: 50px;
   opacity: 1;
-  // margin-left: 80px;
   text-align: center;
   font: normal normal 900 16px/22px;
-  color: #ffffff;
-  &:hover {
-    opacity: 0.8;
-  }
-  &:active {
-    opacity: 0.4;
-  }
   .icon {
     margin-right: 0.25rem;
   }
