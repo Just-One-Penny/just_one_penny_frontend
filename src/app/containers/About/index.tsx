2076ee151a0b8f2a3620c8d490de9dd185ec7867
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2021-01-18 09:23:38 -0500

    About Page styling

--- src/app/containers/About/index.tsx
+++ src/app/containers/About/index.tsx
@@ -173,11 +173,12 @@ const AboutSection = styled.section`
 
 const AboutContent = styled.div`
   flex: 0 0 40.83333333%;
-  margin-top: 4rem;
-  margin-bottom: 6rem;
-  padding: 1.75rem 6rem;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  padding: 1.75rem 4rem;
   background-color: #ffffff;
   border-radius: 5px;
+  font-family: Avenir;
 
   @media only screen and (max-width: 1024px) {
     padding: 1.75rem 3rem;
